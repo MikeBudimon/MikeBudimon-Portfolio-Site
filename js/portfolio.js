77cb9cacd23345881bc5c6faaa77edec9cae369b
Michal Wagner <mikebudimon@gmail.com>
2017-07-28 00:28:27 +0200

    added animated scrolling navigation bar

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,19 +1,35 @@
-// Add smooth scrolling on all links inside the navbar
-$('#exCollapsingNavbar a, #willkommen a').on('click', function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== '') {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+$(document).ready(function () {
+    /**
+     * Smooth scrolling on links
+     */
+    var scrollLink = $('#exCollapsingNavbar a, #willkommen a');
+    scrollLink.on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== '') {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-        // Store hash
-        var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top - 50,
-        }, 800, function () {
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-        });
-    }
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top,
+            }, 800, function () {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    /**
+     * Animated scrolling navigation bar
+     */
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+            $('#navbar-main').addClass('navbar-scroll');
+        } else {
+            $('#navbar-main').removeClass('navbar-scroll');
+        }
+    });
 });
