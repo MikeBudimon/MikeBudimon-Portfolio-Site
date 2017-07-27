60a73078c7862e263aa99ca44880c651eea90384
Michal Wagner <mikebudimon@gmail.com>
2017-07-27 17:54:48 +0200

    small smoth scrolling changes

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,7 +8,6 @@ $('#exCollapsingNavbar a, #willkommen a').on('click', function (event) {
         // Store hash
         var hash = this.hash;
 
-        // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
             scrollTop: $(hash).offset().top - 60,
