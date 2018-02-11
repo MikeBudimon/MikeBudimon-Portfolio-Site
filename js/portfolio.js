dc205f431b0982ac87b586e15df91b4d63db6c8a
Michal Wagner <mikebudimon@gmail.com>
2018-02-11 21:58:39 +0100

    Updated animeted scrolling navigation bar to use vanilla js, no jQuery

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,39 +1,45 @@
-$(document).ready(function () {
-    /**
-     * Smooth scrolling on links
-     */
-    var scrollLink = $('#exCollapsingNavbar a, #start, #scroll-to-top-link');
-    scrollLink.on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== '') {
-            // Prevent default anchor click behavior
-            event.preventDefault();
+document.addEventListener("DOMContentLoaded", function(event) {
+  /**
+   * Smooth scrolling on links
+   */
+  var scrollLink = $("#exCollapsingNavbar a, #start, #scroll-to-top-link");
+  scrollLink.on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-            // Store hash
-            var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top,
-            }, 800, function () {
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
         }
-    });
+      );
+    }
+  });
 
-    /**
-     * Animated scrolling navigation bar
-     */
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > 50) {
-            $('#navbar-main').addClass('navbar-scroll');
-            $('#navbar-main').addClass('box-shadow');
-            document.getElementById('scroll-to-top-link').style.display = 'block';
-        } else {
-            $('#navbar-main').removeClass('navbar-scroll');
-            $('#navbar-main').removeClass('box-shadow');
-            document.getElementById('scroll-to-top-link').style.display = 'none';
-        }
-    });
+  /**
+   * Animated scrolling navigation bar
+   */
+  document.body.onscroll = function() {
+    var scrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop > 50) {
+      document.getElementById("navbar-main").classList.add("navbar-scroll");
+      document.getElementById("navbar-main").classList.add("box-shadow");
+      document.getElementById("scroll-to-top-link").style.display = "block";
+    } else {
+      document.getElementById("navbar-main").classList.remove("navbar-scroll");
+      document.getElementById("navbar-main").classList.remove("box-shadow");
+      document.getElementById("scroll-to-top-link").style.display = "none";
+    }
+  };
 });
